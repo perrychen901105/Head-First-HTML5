7a6d0a39dc62b3eb14f878e7bd9a1b54c8dc404f
Perry <chenzhipeng901105@gmail.com>
2017-01-04 18:09:17 +0800

    add bird image

--- HTML5chapter7/testCanvas.js
+++ HTML5chapter7/testCanvas.js
@@ -16,7 +16,14 @@ function previewHandler() {
 		for (var squares = 0; squares < 20; squares++) {
 			drawSquare(canvas, context);
 		}
+	} else if (shape == "circles") {
+		for (var circles = 0; circles < 20; circles++) {
+			
+			drawCircle(canvas, context);
+		}
 	}
+	drawText(canvas, context);
+	drawBird(canvas, context);
 }
 
 function drawSquare(canvas, context) {
@@ -28,10 +35,69 @@ function drawSquare(canvas, context) {
 	context.fillRect(x,y,w,w);
 }
 
+function drawBird(canvas, context) {
+	var twitterBird = new Image();
+	twitterBird.src = "twitterBird.png";
+	context.drawImage(twitterBird, 20, 120, 70, 70);
+	twitterBird.onload = function() {
+		context.drawImage(twitterBird, 20, 120, 70, 70);
+	};
+}
+
+function drawCircle(canvas, context) {
+	var radius = Math.floor(Math.random() * 40);
+	var x = Math.floor(Math.random() * canvas.width);
+	var y = Math.floor(Math.random() * canvas.height);
+	
+	context.beginPath();
+	context.arc(x, y, radius, 0, degreesToRadians(360), true);
+	
+	context.fillStyle = "lightblue";
+	context.fill();
+
+}
+
+function drawText(canvas, context) {
+	var selectObj = document.getElementById("foregroundColor");
+	var index = selectObj.selectedIndex;
+	var fgColor = selectObj[index].value;
+	
+	context.fillStyle = fgColor;
+	context.font = "bold 1em sans-serif";
+	context.textAlign = "left";
+	context.fillText("I saw this tweet", 20 ,40);
+	
+	selectObj = document.getElementById("tweets");
+	index = selectObj.selectedIndex;
+	context.font = "italic 1.2em serif";
+	context.fillText("tweet", 30, 100);
+	
+	
+	context.font = "bold 1em sans-serif";
+	context.textAlign = "right";
+	context.fillText("and all I got was this lousy t-shirt!", canvas.width - 20, canvas.height-40);
+}
+function degreesToRadians(degrees) {
+    return (degrees * Math.PI)/180;
+}
 function fillBackgroundColor(canvas, context) {
 	var selectObj = document.getElementById("backgroundColor");
 	var index = selectObj.selectedIndex;
 	var bgColor = selectObj.options[index].value;
 	context.fillStyle = bgColor;
 	context.fillRect(0,0,canvas.width,canvas.height);
+}
+function updateTweets(tweets) {
+	var tweetsSelection = document.getElementById("tweets");
+	
+	for (var i = 0; i < tweets.length; i++) {
+		tweet = tweets[i];
+		var option = document.createElement("option");
+		option.text = tweet.text;
+		option.value = tweet.text.replace("\"", "'");
+		
+		tweetsSelection.options.add(option);
+	}
+	
+	tweetsSelection.selectedIndex = 0;
 }
